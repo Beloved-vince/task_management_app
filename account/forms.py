a43b48cbe7072e02d253235996886f10c936ffcc
Beloved-vince <vinceoludare@gmail.com>
2023-04-02 09:26:57 +0100

    docs

--- account/forms.py
+++ account/forms.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 
 class UserForm(forms.ModelForm):
+    """Store form using forms model parent class \
+        username: maximum length of 100
+        email: email field
+        password: Password input"""
     username = forms.CharField(max_length=100)
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput())

2d6ca2e53a841e14f44bb2648c5711d04a073463
Beloved-vince <vinceoludare@gmail.com>
2023-05-22 22:23:49 +0100

    api clall

--- account/views.py
+++ account/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-import dns.resolver
+from account.forms import UserForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .forms import UserForm
 from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth import authenticate, login
 
 
 def create_user(request):
@@ -17,47 +18,39 @@ def create_user(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            domain = email.split('@')[1]
             try:
-                mx_records = dns.resolver.query(domain, 'MX')
-                mx_record = str(mx_records[0].exchange)[:1]
-                if mx_record:
-                    user = User.objects.create_user(username, email, password)
-                    user.save()
-                    messages.success(request, 'Your account has been created')
-                    return redirect('login-in')
-                else:
-                    messages.error(request, 'Your email entered is incorrect')
-                    return HttpResponse("email entered is incorrect")
+                cd = form.cleaned_data
+                username = cd['username']
+                email = cd['email']
+                password = cd['password']
+                user = User.objects.create(
+                    username = username,
+                    email=email,
+                )
+                user.set_password(password)
+                user.save()
+                return JsonResponse({'message': 'success'})
             except Exception as e:
-                messages.error(request, f'try again   : {e}')
-                return HttpResponse(f'{e}')
-
-    # any additional processing or redirecting you want to do
-    return render(request, 'signup.html')
+                messages.error('message', e)
+# any additional processing or redirecting
+    return render(request, 'index.html')
 
 def login_view(request):
     """Login view check if input data is authenticated,\
         allow login if true else do nothing
     """
-    from django.contrib.auth import authenticate, login
 
     if request.method == 'POST':
         try:
             username = request.POST['username']
             password = request.POST['password']
             user = authenticate(username=username, password=password)
+            print (username + password)
             if user is not None:
                 login(request, user)
-                return redirect('add-task')
-            else:
-                messages.error(request, 'wrong username or password')
-        except AttributeError:
-            from django.contrib import messages
-            messages.error(request, "Username or password is incorrect")
-            # return redirect('sign-up')
+                return render(request, 'notes_dashboard.html')
+            messages.error(request, 'username or password is incore')
+        except AttributeError as A:
+            return HttpResponse(A)
 
-    return render(request, 'login.html')
+    return render(request, 'index.html')
